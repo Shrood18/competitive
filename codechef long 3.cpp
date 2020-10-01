c0940fe0d00b6e2be4293ed28105c451ddef6fe9
pinkman18 <72201509+pinkman18@users.noreply.github.com>
2020-10-01 16:51:20 +0530

    Add files via upload

---
+++ codechef long 3.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	long long int t;
+	cin>>t;
+	while(t--)
+	{
+	long long int c,r;
+	cin>>c>>r;
+	int q=ceil(c/9);
+	int w=ceil(w/9);
+    if(q>=w)
+	  cout<<"1"<<" "<<w<<endl;
+	else if(w>q)
+	  cout<<"0"<<" "<<q<<endl;
+     
+}
+
+}
