359e132968d27e1c1776af5a26912e9aad7735dc
pinkman18 <72201509+pinkman18@users.noreply.github.com>
2020-10-01 16:53:02 +0530

    Add files via upload

---
+++ codechef long 2.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		long long int n,k,c=999999999,p;
+		cin>>n>>k;
+		int arr[n];
+		for(int i=0;i<n;i++)
+		  cin>>arr[i];
+		  long long int ans=1;
+		for(int i=0;i<n;i++)
+		  {
+		  	if(k>=arr[i] && k%arr[i]==0)
+		    {
+		       if(c>(k/arr[i]))
+			    {
+				 c=k/arr[i];
+				 p=i;
+		         ans=0;
+			     }
+		
+		      }
+		  }
+		  if(ans==0)
+		  cout<<arr[p]<<endl;
+		  else 
+		  cout<<"-1"<<endl;
+		
+	}
+}
